1645e1ef9cbb8c554e2f7758f6f255196638a35c
rhaynes3k <rhaynes3k@gmail.com>
2020-12-29 14:53:46 -0600

    Intital Commit

---
+++ TODO.md
@@ -0,0 +1,25 @@
+BdayLister Plan
+
+GET /signup
+  Sign up
+
+POST /users
+  creates user in db
+
+GET /login
+  login form
+  submit creates session and redirect
+
+GET /bdays/new
+  form to add a bday
+
+POST /bdays
+  save new bday to db
+
+GET /bdays
+  shows list of user's bdays
+
+GET /bdays/:id
+  shows details of specific bday
+  SHARE BDAY
+    button to send bday to another user
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    # raise "Who dis?".inspect
+    @user = User.new(user_params)
+    @user.name = user_params[:name]
+    @user.email = user_params[:email]
+    @user.birthday = user_params[:birthday]
+    @user.save
+    redirect_to user_path(@user)
+  end
+
+  def user_params
+    params.permit(:name, :email, :birthday)
+  end
+
+end
